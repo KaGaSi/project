563580a54a5030621ac5e132fef7ab8b39ae52ac
Karel Šindelka <karel.sindelka@natur.cuni.cz>
2015-12-14 11:03:02 +0100

    TransformVsf - simple verbose output

--- TransformVsf.c
+++ TransformVsf.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "CStructs.h"
 #include "Structure.h"
 
 void ErrorHelp(char cmd[50]) { //{{{
-    fprintf(stderr, "Usage:\n");
-    fprintf(stderr, "   TransformVsf <output.vsf> <options>\n\n");
-    fprintf(stderr, "   <output.vsf>    output structure file (*.vsf)\n");
-    fprintf(stderr, "   <options>\n");
-    fprintf(stderr, "      -v   verbose output\n");
-    fprintf(stderr, "      -h   print this help and exit\n");
-    fprintf(stderr, "      -i   use input .vsf file different from dl_meso.vsf\n\n");
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, "   TransformVsf <output.vsf> <options>\n\n");
+  fprintf(stderr, "   <output.vsf>    output structure file (*.vsf)\n");
+  fprintf(stderr, "   <options>\n");
+  fprintf(stderr, "      -v   verbose output\n");
+  fprintf(stderr, "      -h   print this help and exit\n");
+  fprintf(stderr, "      -i   use input .vsf file different from dl_meso.vsf\n\n");
 } //}}}
 
 int main(int argc, char *argv[]) {
@@ -89,6 +90,16 @@ int main(int argc, char *argv[]) {
     strcpy(input, "dl_meso.vsf");
   } //}}}
 
+  // -v option - verbose output //{{{
+  bool verbose = false;
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-v") == 0) {
+      verbose = true;
+
+      break;
+    }
+  } //}}}
+
   // variables - structures
   BeadType *BeadType; // structure with info about all bead types
   MoleculeType *MoleculeType; // structure with info about all molecule types
@@ -100,7 +111,7 @@ int main(int argc, char *argv[]) {
   ReadStructure(input, &Counts, &BeadType, &Bead, &MoleculeType, &Molecule);
 
   // print information read from FIELD if option '-v' is used //{{{
-  if (argc > 1 && strcmp(argv[1], "-v") == 0) {
+  if (verbose) {
     printf("\n   Read from FIELD\n\n");
     printf("Counts.{");
     printf("TypesOfBeads =%3d, ", Counts.TypesOfBeads);
@@ -108,7 +119,7 @@ int main(int argc, char *argv[]) {
     printf("Unboded =%7d, ", Counts.Unbonded);
     printf("TypesOfMolecules =%3d, ", Counts.TypesOfMolecules);
     printf("Molecules =%4d}\n", Counts.Molecules);
-    printf("total number of beads: %d\n\n", Counts.Bonded+Counts.Unbonded);
+    printf("\ntotal number of beads: %d\n\n", Counts.Bonded+Counts.Unbonded);
 
     for (int i = 0; i < Counts.TypesOfBeads; i++) {
       printf("BeadType[%2d].{", i);
@@ -125,34 +136,7 @@ int main(int argc, char *argv[]) {
       printf(", Number =%4d", MoleculeType[i].Number);
       printf(", nBeads =%3d", MoleculeType[i].nBeads);
       printf(", nBonds =%3d", MoleculeType[i].nBonds);
-      printf(", Bonds = {");
-      for (int j = 0; j < MoleculeType[i].nBonds; j++) {
-        // print incremented ids, because in FIELD they start from 1
-        printf("%3d %3d", MoleculeType[i].Bond[j][0]+1, MoleculeType[i].Bond[j][1]+1);
-        if (j != (MoleculeType[i].nBonds-1))
-          printf(", ");
-      }
-      printf("}}\n");
-    }
-  } //}}}
-
-  if (argc > 1 && strcmp(argv[1], "-v") == 0) { //{{{
-    printf("\n");
-    for (int i = 0; i < (Counts.Bonded+Counts.Unbonded); i++) {
-      printf("Bead[%6d].Type =%3d\n", i, Bead[i].Type);
-    }
-
-    for (int i = 0; i < Counts.Molecules; i++) {
-      printf("Molecule[%4d].{Type =%3d, Bead = {", i, Molecule[i].Type);
-
-      for (int j = 0; j < MoleculeType[Molecule[i].Type].nBeads; j++) {
-        printf("%6d", Molecule[i].Bead[j]);
-
-        if (j != (MoleculeType[Molecule[i].Type].nBeads-1)) {
-          printf(", ");
-        }
-      }
-      printf("}}\n");
+      printf(", Bonds in %s}\n", output);
     }
   } //}}}
 
