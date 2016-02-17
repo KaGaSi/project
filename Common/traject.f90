fb19fabbf069574cec7fc7b86a9212d66eb8ee3e
Karel Šindelka <karel.sindelka@natur.cuni.cz>
2016-02-17 16:35:16 +0100

    used traject utility from dl_meso v2.6

--- Common/traject.f90
+++ Common/traject.f90
@@ -6,30 +6,29 @@ PROGRAM traject
 !     files created from dl_meso_dpd to produce trajectory file
 !
 !     copyright stfc daresbury laboratory
-!     author - m. a. seaton april 2012
-!
-!     xyz trajectory file modification (M. Lisal, March 2013)
+!     author - m. a. seaton october 2015
 !
 !***********************************************************************
 
       IMPLICIT none
       INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND (15, 307)
-      INTEGER, PARAMETER :: nuser=5,nrtout=9,ntraj=10
+      INTEGER, PARAMETER :: nuser=5,nrtout=9,ntraj=11
 
       CHARACTER(80) :: text
-      CHARACTER(8), ALLOCATABLE :: namspe (:), nammol (:)
-      CHARACTER(8) :: name1
-      CHARACTER(4) :: chan
+      CHARACTER(8), ALLOCATABLE :: namspe (:)
+      CHARACTER(8) :: name1, nammol
+      CHARACTER(6) :: chan
 
       INTEGER, ALLOCATABLE :: ltp (:), ltm (:), mole (:), bndtbl (:,:), beads (:), bonds (:), nspe (:)
-      INTEGER :: chain, ispe, imol, ibond, imxspe, ioerror, i, numtraj, itime, j, k, first, last, nmoldef
+      INTEGER :: chain, ibond, imxspe, ioerror, i, j, k, nmoldef
       INTEGER :: maxnum, numspe, numnodes, nbeads, nubeads, numbeads, nbonds, numbonds, global, species, molecule
-      INTEGER :: steps, lfrzn
+      INTEGER :: lfrzn, keytrj, srfx, srfy, srfz
 
-      REAL(KIND=dp), ALLOCATABLE :: xxx (:), yyy (:), zzz (:), amass (:), bbb (:)
-      REAL(KIND=dp) :: volm, dimx, dimy, dimz, ndx, ndy, ndz, time, mbeads, mglobal, x, y, z, vx, vy, vz
+      REAL(KIND=dp), ALLOCATABLE :: bbb (:)
+      REAL(KIND=dp) :: volm, dimx, dimy, dimz, halfx, halfy, halfz, shrdx, shrdy, shrdz, amass
+      REAL(KIND=dp) :: ndx, ndy, ndz, time, mbeads, mglobal, x, y, z, vx, vy, vz, fx, fy, fz
 
-      LOGICAL :: volchange, eof, bigend
+      LOGICAL :: volchange, eof
 
 !     get number of nodes
 
@@ -43,10 +42,12 @@ PROGRAM traject
 
       ALLOCATE (beads (numnodes), bonds (numnodes))
 
+!     CALL get_command_argument (2, text)
+
 !     determine if HISTORY files exist
 
       IF (numnodes>1) THEN
-        INQUIRE (file = 'HISTORY0000', EXIST = eof)
+        INQUIRE (file = 'HISTORY000000', EXIST = eof)
       ELSE
         INQUIRE (file = 'HISTORY', EXIST = eof)
       END IF
@@ -56,72 +57,27 @@ PROGRAM traject
         STOP
       END IF
 
-!     determine endianness of HISTORY files
+!     scan HISTORY file for process 0 to determine species/molecule names and properties
 
       IF (numnodes>1) THEN
-!       OPEN (ntraj, file = 'HISTORY0000', access = 'sequential', form = 'unformatted', &
-!                   &status = 'unknown', convert = 'big_endian')
-        OPEN (ntraj, file = 'HISTORY0000', access = 'sequential', form = 'unformatted', & ! ML
-                    &status = 'unknown', convert = 'native')
+        OPEN (ntraj, file = 'HISTORY000000', access = 'sequential', form = 'unformatted', status = 'unknown')
       ELSE
- !      OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', &
- !                  &status = 'unknown', convert = 'big_endian')
-        OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', & ! ML
-                    &status = 'unknown', convert = 'native')
-      END IF
-
-      READ (ntraj, IOSTAT=ioerror) numspe, nmoldef, nubeads, numbeads, nbeads, nbonds
-      READ (ntraj, IOSTAT=ioerror) dimx, dimy, dimz, volm
-
-      IF (ioerror/=0) THEN
-        bigend = .false.
-      ELSE
-        bigend = .true.
-      END IF
-
-      CLOSE (ntraj)
-
-!     scan HISTORY file for process 0 to determine species/molecule names
-!     and properties, then find number of available timesteps
-
-      IF (numnodes>1) THEN
-        IF (bigend) THEN
-!         OPEN (ntraj, file = 'HISTORY0000', access = 'sequential', form = 'unformatted', &
-!                     &status = 'unknown', convert = 'big_endian')
-          OPEN (ntraj, file = 'HISTORY0000', access = 'sequential', form = 'unformatted', & ! ML
-                      &status = 'unknown', convert = 'native')
-        ELSE
-!         OPEN (ntraj, file = 'HISTORY0000', access = 'sequential', form = 'unformatted', &
-!                     &status = 'unknown', convert = 'little_endian')
-          OPEN (ntraj, file = 'HISTORY0000', access = 'sequential', form = 'unformatted', & ! ML
-                      &status = 'unknown', convert = 'native')
-        END IF
-      ELSE
-        IF (bigend) THEN
-!         OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', &
-!                     &status = 'unknown', convert = 'big_endian')
-          OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', & ! ML
-                      &status = 'unknown', convert = 'native')
-        ELSE
-!         OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', & ! ML
-!                     &status = 'unknown', convert = 'little_endian')
-          OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', &
-                      &status = 'unknown', convert = 'native')
-        END IF
+        OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', status = 'unknown')
       END IF
 
       READ (ntraj) numspe, nmoldef, nubeads, numbeads, nbeads, nbonds
       READ (ntraj) dimx, dimy, dimz, volm
+      READ (ntraj) keytrj, srfx, srfy, srfz
 
-      ALLOCATE (namspe (numspe), nammol (nmoldef))
-      ALLOCATE (amass (numspe), bbb (numspe))
+      ALLOCATE (namspe (numspe))
+      ALLOCATE (bbb (numspe))
 
       DO i = 1, numspe
-        READ (ntraj) namspe (i), amass (i), bbb (i), lfrzn
+        READ (ntraj) namspe (i), amass, bbb (i), lfrzn
       END DO
 
       DO i = 1, nmoldef
-        READ (ntraj) nammol (i)
+        READ (ntraj) nammol
       END DO
 
       READ (ntraj) text
@@ -136,30 +92,7 @@ PROGRAM traject
         END DO
       END IF
 
-      numtraj = 0
-
-      DO WHILE (.true.)
-
-        READ (ntraj, IOSTAT=ioerror) time, mbeads, ndx, ndy, ndz
-
-        IF (ioerror/=0) THEN
-          EXIT
-        ELSE
-          numtraj = numtraj + 1
-          nbeads = NINT (mbeads)
-          DO i = 1, nbeads
-            READ (ntraj) mglobal, x, y, z, vx, vy, vz
-          END DO
-        END IF
-
-      END DO
-
       CLOSE (ntraj)
-
-      IF (numtraj==0) THEN
-        PRINT *, 'ERROR: cannot find trajectory data in HISTORY files'
-        STOP
-      END IF
 
 !     read in and initialize data, including numbers of beads and bonds
 
@@ -167,35 +100,16 @@ PROGRAM traject
 
       DO j = 1, numnodes
 
-        WRITE (chan, '(i4.4)') j-1
+        WRITE (chan, '(i6.6)') j-1
         IF (numnodes>1) THEN
-          IF (bigend) THEN
-!           OPEN ((ntraj+j-1), file = 'HISTORY'//chan, access = 'sequential', form = 'unformatted', &
-!                             &status = 'unknown', convert = 'big_endian')
-            OPEN ((ntraj+j-1), file = 'HISTORY'//chan, access = 'sequential', form = 'unformatted', & ! ML
-                              &status = 'unknown', convert = 'native')
-          ELSE
- !          OPEN ((ntraj+j-1), file = 'HISTORY'//chan, access = 'sequential', form = 'unformatted', &
- !                            &status = 'unknown', convert = 'little_endian')
-            OPEN ((ntraj+j-1), file = 'HISTORY'//chan, access = 'sequential', form = 'unformatted', & ! ML
-                              &status = 'unknown', convert = 'native')
-          END IF
+          OPEN ((ntraj+j-1), file = 'HISTORY'//chan, access = 'sequential', form = 'unformatted', status = 'unknown')
         ELSE
-          IF (bigend) THEN
-!           OPEN ((ntraj+j-1), file = 'HISTORY', access = 'sequential', form = 'unformatted', &
-!                             &status = 'unknown', convert = 'big_endian')
-            OPEN ((ntraj+j-1), file = 'HISTORY', access = 'sequential', form = 'unformatted', & ! ML
-                              &status = 'unknown', convert = 'native')
-          ELSE
-!           OPEN ((ntraj+j-1), file = 'HISTORY', access = 'sequential', form = 'unformatted', &
-!                             &status = 'unknown', convert = 'little_endian')
-            OPEN ((ntraj+j-1), file = 'HISTORY', access = 'sequential', form = 'unformatted', & ! ML
-                              &status = 'unknown', convert = 'native')
-          END IF
+          OPEN (ntraj, file = 'HISTORY', access = 'sequential', form = 'unformatted', status = 'unknown')
         END IF
 
         READ (ntraj+j-1) numspe, nmoldef, nubeads, numbeads, nbeads, nbonds
         READ (ntraj+j-1) dimx, dimy, dimz, volm
+        READ (ntraj+j-1) keytrj, srfx, srfy, srfz
 
         beads (j) = nbeads
         bonds (j) = nbonds
@@ -208,7 +122,7 @@ PROGRAM traject
       DO j = 1, numnodes
 
         DO i = 1, numspe
-          READ (ntraj+j-1) name1, vx, vy
+          READ (ntraj+j-1) name1, vx, vy, lfrzn
         END DO
 
         DO i = 1, nmoldef
@@ -221,11 +135,6 @@ PROGRAM traject
 
 !     get number of beads to include in trajectory file
 
-      first = 1
-      last = numbeads
-      steps = numtraj
-
-      ALLOCATE (xxx (first:last), yyy (first:last), zzz (first:last))
       ALLOCATE (ltp (1:numbeads), ltm (1:numbeads), mole (1:numbeads), nspe (numspe))
 
 !     read in data for beads and bonds
@@ -240,12 +149,10 @@ PROGRAM traject
 
         DO i = 1, nbeads
           READ (ntraj+j-1) global, species, molecule, chain
-          IF (global>=first .AND. global<=last) THEN
-            ltp (global) = species
-            ltm (global) = molecule
-            mole (global) = chain
-            nspe (species) = nspe (species) + 1
-          END IF
+          ltp (global) = species
+          ltm (global) = molecule
+          mole (global) = chain
+          nspe (species) = nspe (species) + 1
         END DO
 
         IF (nbonds>0) THEN
@@ -257,8 +164,11 @@ PROGRAM traject
 
       END DO
 
+      DEALLOCATE (beads, bonds)
+
 !     write output file (.VTF for VMD)
 
+!     OPEN (nrtout, file = 'traject.vtf')
       OPEN (nrtout, file = 'dl_meso.vsf')
 
       maxnum = 0
@@ -271,15 +181,15 @@ PROGRAM traject
 
 !     define beads
 
-      IF ((last-first+1)==maxnum) THEN
-        WRITE (nrtout, '("atom 0:",I8.8,"    radius ",F10.6," name ",A8)') last-first, bbb (imxspe), namspe (imxspe)
+      IF (numbeads==maxnum .AND. numbonds==0) THEN
+        WRITE (nrtout, '("atom 0:",I8.8,"    radius ",F10.6," name ",A8)') numbeads-1, bbb (imxspe), namspe (imxspe)
       ELSE
         WRITE (nrtout, '("atom default    radius ",F10.6," name ",A8)') bbb (imxspe), namspe (imxspe)
-        DO i = first, last
-          IF ((ltp (i)/=imxspe .OR. i==last) .AND. ltm(i)==0) THEN
-            WRITE (nrtout, '("atom ",I8,"    radius ",F10.6," name ",A8)') i-first, bbb (ltp (i)), namspe (ltp (i))
+        DO i = 1, numbeads
+          IF ((ltp (i)/=imxspe .OR. i==numbeads) .AND. ltm(i)==0) THEN
+            WRITE (nrtout, '("atom ",I8,"    radius ",F10.6," name ",A8)') i-1, bbb (ltp (i)), namspe (ltp (i))
           ELSE IF (ltm (i)/=0) THEN
-            WRITE (nrtout, '("atom ",I8,"    radius ",F10.6," name ",A8," resid ",I8)') i-first, bbb (ltp (i)), &
+            WRITE (nrtout, '("atom ",I8,"    radius ",F10.6," name ",A8," resid ",I8)') i-1, bbb (ltp (i)), &
                   &namspe (ltp (i)), mole (i)
           END IF
         END DO
@@ -287,72 +197,97 @@ PROGRAM traject
 
 !     define bonds
 
-      IF (numbonds>0 .AND. last>nubeads) THEN
+      IF (numbonds>0 .AND. numbeads>nubeads) THEN
         WRITE (nrtout, '()')
         DO i = 1, numbonds
-          IF ((bndtbl (i, 1)-first)>=0) WRITE (nrtout, '("bond ",I8,":",I8)') (bndtbl (i, 1)-first), (bndtbl (i, 2)-first)
+          IF ((bndtbl (i, 1)-1)>=0) WRITE (nrtout, '("bond ",I8,":",I8)') (bndtbl (i, 1)-1), (bndtbl (i, 2)-1)
         END DO
       END IF
 
       CLOSE (nrtout)
 
+      DEALLOCATE (ltp, ltm, mole, bndtbl, nspe, namspe, bbb)
+
+      OPEN (nrtout, file = 'All.vcf')
+
 !     obtain positions and velocities for all beads at each time step
 
       volchange = .true.
       eof = .false.
+      k = 0
 
-      OPEN (nrtout, file = 'All.vcf')
+      DO WHILE (.true.)
 
-      WRITE (nrtout, '("pbc ", 3F10.6)') dimx, dimy, dimz
+        READ (ntraj, IOSTAT=ioerror) time, mbeads, ndx, ndy, ndz, shrdx, shrdy, shrdz
 
-      DO k = 1, steps
+        IF (ioerror/=0) THEN
+          eof = .true.
+          IF (k==0) THEN
+            PRINT *, 'ERROR: cannot find trajectory data in HISTORY files'
+            STOP
+          END IF
+          EXIT
+        END IF
+
+        k = k + 1
+        volchange = (ndx/=dimx .OR. ndy/=dimy .OR. ndz/=dimz)
+        dimx = ndx
+        dimy = ndy
+        dimz = ndz
+!        halfx = 0.5_dp * dimx
+!        halfy = 0.5_dp * dimy
+!        halfz = 0.5_dp * dimz
+        halfx = 0.0_dp
+        halfy = 0.0_dp
+        halfz = 0.0_dp
+
+        IF (k==1) THEN
+          WRITE (nrtout, '("pbc ", 3F10.6,//,"#"(I10),/,"timestep")') dimx, dimy, dimz, k
+        ELSE
+          WRITE (nrtout, '(/,"#",(I6),/,"timestep")') k
+        END IF
 
         DO j = 1, numnodes
 
-          READ (ntraj+j-1, IOSTAT=ioerror) time, mbeads, ndx, ndy, ndz
-
-          IF (ioerror/=0) THEN
-            PRINT *, 'ERROR: End of file reached prematurely - ', k-1, ' timesteps written to VTF file'
-            eof = .true.
-            EXIT
+          IF (j>1) THEN
+            READ (ntraj+j-1, IOSTAT=ioerror) time, mbeads, ndx, ndy, ndz, shrdx, shrdy, shrdz
+            IF (ioerror/=0) THEN
+              PRINT *, 'ERROR: End of file reached prematurely - ', k-1, ' timesteps written to VTF file'
+              eof = .true.
+              EXIT
+            END IF
           END IF
 
-          nbeads = NINT (mbeads)
-          volchange = (ndx/=dimx .OR. ndy/=dimy .OR. ndz/=dimz)
-          dimx = ndx
-          dimy = ndy
-          dimz = ndz
-
-          DO i = 1, nbeads
-
-            READ (ntraj+j-1) mglobal, x, y, z, vx, vy, vz
-            global = NINT (mglobal)
-
-            IF (global>=first .AND. global <=last) THEN
-              xxx (global) = x
-              yyy (global) = y
-              zzz (global) = z
-            END IF
-
-          END DO
-
           IF (eof) EXIT
+
+          nbeads = NINT (mbeads)
+
+          SELECT CASE (keytrj)
+          CASE (0)
+            DO i = 1, nbeads
+              READ (ntraj+j-1) mglobal, x, y, z
+              global = NINT (mglobal)
+              WRITE (nrtout, '(3F7.3)')  x-halfx, y-halfy, z-halfz
+            END DO
+          CASE (1)
+            DO i = 1, nbeads
+              READ (ntraj+j-1) mglobal, x, y, z, vx, vy, vz
+              global = NINT (mglobal)
+              WRITE (nrtout, '(3F7.3)')  x-halfx, y-halfy, z-halfz
+            END DO
+          CASE (2)
+            DO i = 1, nbeads
+              READ (ntraj+j-1) mglobal, x, y, z, vx, vy, vz, fx, fy, fz
+              global = NINT (mglobal)
+              WRITE (nrtout, '(3F7.3)')  x-halfx, y-halfy, z-halfz
+            END DO
+          END SELECT
 
         END DO
 
         IF (eof) EXIT
 
-        IF (volchange .OR. k==1) THEN
-          WRITE (nrtout, '(/,"#",(I6),/,"timestep")') k
-        ELSE
-          WRITE (nrtout, '(/,"#",(I6),/,"timestep")') k
-        END IF
-
-        DO i = first, last
-          WRITE (nrtout, '(3F8.3)') xxx (i), yyy (i), zzz (i)
-        END DO
-
-        PRINT *, "Timestep ",k,"/",numtraj," : time = ",time
+        PRINT *, "Timestep ",k," : time = ",time
 
       END DO
 
@@ -362,7 +297,7 @@ PROGRAM traject
         CLOSE (ntraj+j-1)
       END DO
 
-CONTAINS
+CONTAINS 
 
       INTEGER FUNCTION parseint (word)
 
