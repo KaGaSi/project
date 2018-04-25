b0ca242610b2ad341f24b344c4875b3a540e374d
Karel Šindelka <k.sindelka@gmail.com>
2018-04-25 18:54:59 +0200

    New ReadStructure
    Bead mass and charge are read from FIELD, buth everything else is read
    from vsf file. After system information is read, it is determined what
    bead types are present in vcf file and all the structures are asjusted
    accordingly.

--- Common/test.c
+++ Common/test.c
@@ -23,16 +23,77 @@ int main(int argc, char *argv[]) {
   printf("vcf file: %s\n", vcf);
   ReadStructure(vsf_file, vcf, bonds_file, &Counts, &BeadType, &Bead, &MoleculeType, &Molecule);
 
-  printf("Bonded=%d Unbonded=%d\n", Counts.Bonded, Counts.Unbonded);
-  printf("Beads in vsf=%d Molecules in vsf=%d\n", Counts.BeadsInVsf, Counts.MoleculesInVsf);
-  printf("Beads=%d Molecules=%d\n", Counts.Beads, Counts.Molecules);
-  printf("Bead types=%d Molecule types=%d\n", Counts.TypesOfBeads, Counts.TypesOfMolecules);
-  for (int i = 0; i < Counts.TypesOfBeads; i++) {
-    printf("%s %d %d\n", BeadType[i].Name, i, BeadType[i].Number);
-  }
-  for (int i = 0; i < Counts.TypesOfMolecules; i++) {
-    printf("%s %d %d %d\n", MoleculeType[i].Name, i, MoleculeType[i].Number, MoleculeType[i].nBeads);
-  }
+//// prints for debugging //{{{
+//// Counts struct
+//printf("Counts.{TypesOfMolecules = %d,", Counts.TypesOfMolecules);
+//printf(" Molecules = %d,", Counts.Molecules);
+//printf(" TypesOfBeads = %d,", Counts.TypesOfBeads);
+//printf(" BeadsInVsf = %d,", Counts.BeadsInVsf);
+//printf(" Beads = %d}\n", Counts.Beads);
+//// MoleculeType struct
+//for (int i = 0; i < Counts.TypesOfMolecules; i++) {
+//  printf("MoleculeType[%d].{Name = %s, ", i, MoleculeType[i].Name);
+//  printf("Number = %d, ", MoleculeType[i].Number);
+//  printf("nBeads = %d, ", MoleculeType[i].nBeads);
+//  printf("nBTypes = %d, BTypes = {", MoleculeType[i].nBTypes);
+//  for (int j = 0; j < MoleculeType[i].nBTypes; j++) {
+//    if (j == 0 && MoleculeType[i].nBTypes == 1) {
+//      printf("%d} ", MoleculeType[i].BType[j]);
+//    } else if (j == 0) {
+//      printf("%d,", MoleculeType[i].BType[j]);
+//    } else if (j < (MoleculeType[i].nBTypes-1)) {
+//      printf(" %d,", MoleculeType[i].BType[j]);
+//    } else {
+//      printf(" %d}, ", MoleculeType[i].BType[j]);
+//    }
+//  }
+//  printf("nBonds = %d, Bond = {", MoleculeType[i].nBonds);
+//  for (int j = 0; j < MoleculeType[i].nBonds; j++) {
+//    if (j == 0 && MoleculeType[i].nBonds == 1) {
+//      printf("%d-%d}", MoleculeType[i].Bond[j][0], MoleculeType[i].Bond[j][1]);
+//    } else if (j == 0) {
+//      printf("%d-%d,", MoleculeType[i].Bond[j][0], MoleculeType[i].Bond[j][1]);
+//    } else if (j < (MoleculeType[i].nBonds-1)) {
+//      printf(" %d-%d,", MoleculeType[i].Bond[j][0], MoleculeType[i].Bond[j][1]);
+//    } else {
+//      printf(" %d-%d}", MoleculeType[i].Bond[j][0], MoleculeType[i].Bond[j][1]);
+//    }
+//  }
+//  printf("}\n");
+//}
+//// BeadType struct
+//for (int i = 0; i < Counts.TypesOfBeads; i++) {
+//  printf("BeadType[%d].{Name = %s, Number = %d, Use = %s}\n", i,
+//                                                             BeadType[i].Name,
+//                                                             BeadType[i].Number,
+//                                                             BeadType[i].Use? "Yes":"No");
+//}
+//// Molecule struct
+//for (int i = 0; i < Counts.Molecules; i++) {
+//  int type = Molecule[i].Type;
+//  printf("Molecule[%d].{Type = %d", i, Molecule[i].Type);
+//  printf(" (%s),", MoleculeType[type].Name);
+//  printf(" Bead (%d) = {", MoleculeType[type].nBeads);
+//  for (int j = 0; j < MoleculeType[type].nBeads; j++) {
+//    if (j == 0 && j == MoleculeType[type].nBeads) {
+//      printf("%d}", Molecule[i].Bead[j]);
+//    } else if (j == 0) {
+//      printf("%d,", Molecule[i].Bead[j]);
+//    } else if (j < (MoleculeType[type].nBeads-1)) {
+//      printf(" %d,", Molecule[i].Bead[j]);
+//    } else {
+//      printf(" %d}", Molecule[i].Bead[j]);
+//    }
+//  }
+//  printf("}\n");
+//}
+//// Bead struct
+//for (int i = 0; i < Counts.Beads; i++) {
+//  printf("Bead[%d].{Type = %d", i, Bead[i].Type);
+//  printf(" (%s),", BeadType[Bead[i].Type].Name);
+//  printf(" Molecule = %d,", Bead[i].Molecule);
+//  printf(" Index = %d}\n", Bead[i].Index);
+//} //}}}
 
   // free memory - to make valgrind happy //{{{
   free(BeadType);
