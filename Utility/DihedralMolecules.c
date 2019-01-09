1778bcaab9ccc3ae9ad067a622d04882cf26756e
Karel Šindelka <k.sindelka@gmail.com>
2019-01-09 12:58:26 +0100

    DihedralMolecules & AngleMolecules improved
    Both utilities now also calculate distribution of angles and overall
    averages. Manual was updated.

--- Utility/DihedralMolecules.c
+++ Utility/DihedralMolecules.c
@@ -9,16 +9,17 @@
 
 void ErrorHelp(char cmd[50]) { //{{{
   fprintf(stderr, "Usage:\n");
-  fprintf(stderr, "   %s <input> <width> <mol name(s)> <options>\n\n", cmd);
+  fprintf(stderr, "   %s <input> <width> <output> <mol name(s)> <options>\n\n", cmd);
 
-  fprintf(stderr, "   <input>                input filename (either vcf or vtf format)\n");
-  fprintf(stderr, "   <width>                width of a single bin\n");
-  fprintf(stderr, "   <mol name(s)>          molecule names to calculate density for\n");
+  fprintf(stderr, "   <input>         input filename (either vcf or vtf format)\n");
+  fprintf(stderr, "   <width>         width of a single bin in degrees\n");
+  fprintf(stderr, "   <output>        output file with distribution of dihedral angles\n");
+  fprintf(stderr, "   <mol name(s)>   molecule names to calculate angles for\n");
   fprintf(stderr, "   <options>\n");
-  fprintf(stderr, "      --joined            specify that <input> contains joined coordinates\n");
-  fprintf(stderr, "      -a <name> <ints>    output file (default: dihedral.txt) and bead inices (4 per molecule type) ");
-  fprintf(stderr, "for dihedral calculation (default: 1 2 3 4)\n");
-  fprintf(stderr, "      -st <int>           starting timestep for calculation\n");
+  fprintf(stderr, "      --joined     specify that <input> contains joined coordinates\n");
+  fprintf(stderr, "      -n <ints>    bead indices (multiple of 6 <ints>) for dihedral calculation (default: 1 2 3 2 3 4)\n");
+  fprintf(stderr, "      -a <name>    write angle of all molecules in all times to <name>\n");
+  fprintf(stderr, "      -st <int>    starting timestep for calculation\n");
   CommonHelp(1);
 } //}}}
 
@@ -29,7 +30,7 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[i], "-h") == 0) {
       fprintf(stdout, "\
 DihedralMolecules utility calculates dihedral angle \
-between for specified beads in each molecule of specified molecule \
+between planes specified by beads in each molecule of specified molecule \
 type(s). \
 \n\n");
 
@@ -40,22 +41,23 @@ system.\n\n");
 */
 
       fprintf(stdout, "Usage:\n");
-      fprintf(stdout, "   %s <input> <width> <mol name(s)> <options>\n\n", argv[0]);
+      fprintf(stdout, "   %s <input> <width> <output>  <mol name(s)> <options>\n\n", argv[0]);
 
-      fprintf(stdout, "   <input>                input filename (either vcf or vtf format)\n");
-      fprintf(stdout, "   <width>                width of a single bin\n");
-      fprintf(stdout, "   <mol name(s)>          molecule names to calculate density for\n");
+      fprintf(stdout, "   <input>         input filename (either vcf or vtf format)\n");
+      fprintf(stdout, "   <width>         width of a single bin in degrees\n");
+      fprintf(stdout, "   <output>        output file with distribution of dihedral angles\n");
+      fprintf(stdout, "   <mol name(s)>   molecule names to calculate angles for\n");
       fprintf(stdout, "   <options>\n");
-      fprintf(stdout, "      --joined            specify that <input> contains joined coordinates\n");
-      fprintf(stdout, "      -a <name> <ints>    output file (default: dihedral.txt) and bead inices (4 per molecule type) ");
-      fprintf(stdout, "for dihedral calculation (default: 1 2 3 4)\n");
-      fprintf(stdout, "      -st <int>           starting timestep for calculation\n");
+      fprintf(stdout, "      --joined     specify that <input> contains joined coordinates\n");
+      fprintf(stdout, "      -n <ints>    bead indices (multiple of 6 <ints>) for dihedral calculation (default: 1 2 3 2 3 4)\n");
+      fprintf(stdout, "      -a <name>    write angle of all molecules in all times to <name>\n");
+      fprintf(stdout, "      -st <int>    starting timestep for calculation\n");
       CommonHelp(0);
       exit(0);
     }
   }
 
-  int req_args = 3; //}}}
+  int req_args = 4; //}}}
 
   // check if correct number of arguments //{{{
   int count = 0;
@@ -81,6 +83,7 @@ system.\n\n");
         strcmp(argv[i], "--script") != 0 &&
         strcmp(argv[i], "--joined") != 0 &&
         strcmp(argv[i], "-a") != 0 &&
+        strcmp(argv[i], "-n") != 0 &&
         strcmp(argv[i], "-st") != 0) {
 
       ErrorOption(argv[i]);
@@ -178,7 +181,14 @@ system.\n\n");
     ErrorHelp(argv[0]);
     exit(1);
   }
-  double width = atof(argv[count]); //}}}
+  double width = atof(argv[count]);
+
+  // number of bins between 0 and 180 deg
+  int bins = ceil(180 / width); //}}}
+
+  // <output> - file name with dihedral angle distribution //{{{
+  char output_distr[32];
+  strcpy(output_distr, argv[++count]); //}}}
 
   // variables - structures //{{{
   BeadType *BeadType; // structure with info about all bead types
@@ -207,19 +217,15 @@ system.\n\n");
     }
   } //}}}
 
-  // '-a' option - specify for bead ids //{{{
-  int dihedral[100] = {0}, number_of_beads = 0;
+  // '-n' option - specify bead ids //{{{
+  int dihedral[100] = {0}, number_of_beads = 6, beads_per_angle = 6, test;
   dihedral[0] = 1; // default planes: 1-2-3 & 2 3 4
   dihedral[1] = 2;
   dihedral[2] = 3;
   dihedral[3] = 2;
-  dihedral[2] = 3;
-  dihedral[3] = 4;
-  number_of_beads = 6;
-  char output[32];
-  int test;
-  strcpy(output, "dihedral.txt"); // default output filename (if '-a' is missing)
-  if (FileIntsOption(argc, argv, "-a", dihedral, &test, output)) {
+  dihedral[4] = 3;
+  dihedral[5] = 4;
+  if (MultiIntegerOption(argc, argv, "-n", &test, dihedral)) {
     exit(1);
   }
   if (test != 0) { // -a is present
@@ -227,7 +233,7 @@ system.\n\n");
   }
 
   // Error: wrong number of integers //{{{
-  if ((number_of_beads%6) != 0) {
+  if ((number_of_beads%beads_per_angle) != 0) {
     fprintf(stderr, "\nError: '-a' option - number of bead ids must be dividable by six.\n");
     exit(1);
   } //}}}
@@ -235,47 +241,59 @@ system.\n\n");
   for (int i = 0; i < number_of_beads; i++) {
     dihedral[i]--; // ids should start with zero
 
-    // Warning - too high id for specific molecule //{{{
+    // Error - too high id for specific molecule //{{{
     for (int j = 0; j < Counts.TypesOfMolecules; j++) {
       if (MoleculeType[j].Use && dihedral[i] >= MoleculeType[j].nBeads) {
-        fprintf(stderr, "\nWarning: '-a' option - %d is larger than the number of beads in molecule %s.\n", dihedral[i], MoleculeType[j].Name);
-        fprintf(stderr, "Series with this bead id will not be used to calculate dihedral angle in %s.\n\n", MoleculeType[j].Name);
+        fprintf(stderr, "\nError: '-a' option - %d is larger than the number of beads in molecule %s\n\n", dihedral[i], MoleculeType[j].Name);
+        ErrorHelp(argv[0]);
+        exit(1);
       }
     } //}}}
   } //}}}
 
-  // open output file and write initial stuff //{{{
-  FILE *out;
-  if ((out = fopen(output, "w")) == NULL) {
-    ErrorFileOpen(output, 'w');
+  // '-a' option - write angles for all molecules //{{{
+  char *output = calloc(32,sizeof(char *));
+  if (FileOption(argc, argv, "-a", &output)) {
     exit(1);
   }
 
-  // print command to output file //{{{
-  putc('#', out);
-  for (int i = 0; i < argc; i++)
-    fprintf(out, " %s", argv[i]);
-  putc('\n', out); //}}}
+  // write initial stuff to output if '-a' is used
+  if (output[0] != '\0') {
+    // open output //{{{
+    FILE *out;
+    if ((out = fopen(output, "w")) == NULL) {
+      ErrorFileOpen(output, 'w');
+      exit(1);
+    } //}}}
 
-  // print molecule names & bead ids //{{{
-  count = 1;
-  fprintf(out, "# (1) step;");
-  for (int i = 0; i < Counts.Molecules; i++) {
-    int mol_type = Molecule[i].Type;
-    if (MoleculeType[mol_type].Use) {
-      for (int j = 0; j < number_of_beads; j += 4) {
-        if (dihedral[j+0] < MoleculeType[mol_type].nBeads &&
-            dihedral[j+1] < MoleculeType[mol_type].nBeads &&
-            dihedral[j+2] < MoleculeType[mol_type].nBeads &&
-            dihedral[j+3] < MoleculeType[mol_type].nBeads) {
-          fprintf(out, " (%d) %d %s:%d-%d-%d-%d;", ++count, i+1, MoleculeType[mol_type].Name, dihedral[j]+1, dihedral[j+1]+1, dihedral[j+2]+1, dihedral[j+3]+1);
+    // print command to output file //{{{
+    putc('#', out);
+    for (int i = 0; i < argc; i++)
+      fprintf(out, " %s", argv[i]);
+    putc('\n', out); //}}}
+
+    // print molecule names & bead ids //{{{
+    fprintf(out, "# dihedral angles between planes specifief by:");
+    for (int j = 0; j < number_of_beads; j += beads_per_angle) {
+      fprintf(out, " (%d) %d-%d-%d & %d-%d-%d;", j/beads_per_angle+1, dihedral[j], dihedral[j+1], dihedral[j+2], dihedral[j+3], dihedral[j+4], dihedral[j+5]);
+    }
+    putc('\n', out);
+    fprintf(out, "# columns: (1) step;");
+    int j = 2;
+    for (int i = 0; i < Counts.TypesOfMolecules; i++) {
+      if (MoleculeType[i].Use) {
+        if ((number_of_beads/beads_per_angle*MoleculeType[i].Number) == 1) {
+          fprintf(out, " (%d) %s molecules;", j, MoleculeType[i].Name);
+        } else {
+          fprintf(out, " (%d) to (%d) %s molecules;", j, j+number_of_beads/beads_per_angle*MoleculeType[i].Number-1, MoleculeType[i].Name);
         }
+        j += number_of_beads / beads_per_angle * MoleculeType[i].Number;
       }
     }
-  }
-  putc('\n', out); //}}}
+    putc('\n', out); //}}}
 
-  fclose(out); //}}}
+    fclose(out);
+  } //}}}
 
   // open input coordinate file //{{{
   FILE *vcf;
@@ -309,10 +327,6 @@ system.\n\n");
     fprintf(stdout, "   box size: %lf x %lf x %lf\n\n", BoxLength.x, BoxLength.y, BoxLength.z);
   } //}}}
 
-  // number of bins //{{{
-  double max_dist = 0.5 * Min3(BoxLength.x, BoxLength.y, BoxLength.z);
-  int bins = ceil(max_dist / width); //}}}
-
   // create array for the first line of a timestep ('# <number and/or other comment>') //{{{
   char *stuff;
   stuff = malloc(128*sizeof(int));
@@ -320,17 +334,6 @@ system.\n\n");
   // initialize the array
   for (int i = 0; i < 128; i++) {
     stuff[i] = '\0';
-  } //}}}
-
-  // allocate Aggregate struct //{{{
-  Aggregate *Aggregate = calloc(Counts.Molecules,sizeof(*Aggregate));
-  for (int i = 0; i < Counts.Molecules; i++) {
-    // assumes all monomeric beads can be near one aggregate - memory-heavy, but reliable
-    Aggregate[i].Monomer = calloc(Counts.Unbonded,sizeof(int));
-    // assumes all bonded beads can be in one aggregate - memory-heavy, but reliable
-    Aggregate[i].Bead = calloc(Counts.Bonded,sizeof(int));
-    // maximum of all molecules can be in one aggregate
-    Aggregate[i].Molecule = calloc(Counts.Molecules,sizeof(int));
   } //}}}
 
   // print information - verbose output //{{{
@@ -382,12 +385,15 @@ system.\n\n");
   } //}}}
   //}}}
 
-  double *distr[Counts.TypesOfMolecules][number_of_beads/2];
+  // allocate array for distribution of angles //{{{
+  double avg_angle[Counts.TypesOfMolecules][number_of_beads/beads_per_angle];
+  double *distr[Counts.TypesOfMolecules][number_of_beads/beads_per_angle];
   for (int i = 0; i < Counts.TypesOfMolecules; i++) {
-    for (int j = 0; j < (number_of_beads/6); j++) {
+    for (int j = 0; j < (number_of_beads/beads_per_angle); j++) {
+      avg_angle[i][j] = 0;
       distr[i][j] = calloc(bins, sizeof(double));
     }
-  }
+  } //}}}
 
   // main loop //{{{
   count = 0; // count timesteps
@@ -417,14 +423,14 @@ system.\n\n");
     } //}}}
 
     // calculate dihedral angles //{{{
-    double angle[Counts.Molecules][number_of_beads/6];
+    double angle[Counts.Molecules][number_of_beads/beads_per_angle];
     for (int i = 0; i < Counts.Molecules; i++) {
       int mol_type = Molecule[i].Type;
       if (MoleculeType[mol_type].Use) {
 
         // calculate normal vectors to specified planes
         // first plane is given by 0 1 2 and second plane by 1 2 3
-        for (int j = 0; j < number_of_beads; j += 6) {
+        for (int j = 0; j < number_of_beads; j += beads_per_angle) {
           // plane normals //{{{
           Vector u[2], v[2], n[2];
           // vectors in first plane (points 0 1 2): u=1-2; v=1-2
@@ -455,36 +461,44 @@ system.\n\n");
           size[0] = sqrt(SQR(n[0].x) + SQR(n[0].y) + SQR(n[0].z));
           size[1] = sqrt(SQR(n[1].x) + SQR(n[1].y) + SQR(n[1].z));
           double scalar = n[0].x * n[1].x + n[0].y * n[1].y + n[0].z * n[1].z;
-          angle[i][j/6] = acos(scalar / (size[0] * size[1])); // in rad
-          angle[i][j/6] *= 180 / PI; // in degrees //}}}
+          angle[i][j/beads_per_angle] = acos(scalar / (size[0] * size[1])); // in rad
+          angle[i][j/beads_per_angle] *= 180 / PI; // in degrees //}}}
 
-          int k = angle[i][j/6] / width;
+          // add to average
+          avg_angle[mol_type][j/beads_per_angle] += angle[i][j/beads_per_angle];
+
+          int k = angle[i][j/beads_per_angle] / width;
           if (k < bins) {
-            distr[mol_type][j/6][k]++;
+            distr[mol_type][j/beads_per_angle][k]++;
+          } else {
+            fprintf(stdout, "\nWarning - weird angle: %lf degrees\n", angle[i][j/beads_per_angle]);
           }
         }
       }
     } //}}}
 
-    // write angles to output file //{{{
-    if ((out = fopen(output, "a")) == NULL) {
-      ErrorFileOpen(output, 'a');
-      exit(1);
-    }
+    // write all angles to output if '-a' is used //{{{
+    if (output[0] != '\0') {
+      FILE *out;
+      if ((out = fopen(output, "a")) == NULL) {
+        ErrorFileOpen(output, 'a');
+        exit(1);
+      }
 
-    fprintf(out, "%6d", count);
-    for (int i = 0; i < Counts.Molecules; i++) {
-      int mol_type = Molecule[i].Type;
-      if (MoleculeType[mol_type].Use) {
-        for (int j = 0; j < number_of_beads; j += 6){
-          fprintf(out, " %10.6f", 180-angle[i][j/6]); // write angle between planes, not normals
+      fprintf(out, "%6d", count);
+      for (int i = 0; i < Counts.Molecules; i++) {
+        int mol_type = Molecule[i].Type;
+        if (MoleculeType[mol_type].Use) {
+          for (int j = 0; j < number_of_beads; j += beads_per_angle){
+            fprintf(out, " %10.6f", 180-angle[i][j/beads_per_angle]); // write angle between planes, not normals
+          }
         }
       }
-    }
-    putc('\n', out);
+      putc('\n', out);
 
-    // write stuff
-    fclose(out); //}}}
+      // write stuff
+      fclose(out);
+    } //}}}
 
     // print comment at the beginning of a timestep - detailed verbose output //{{{
     if (verbose2) {
@@ -502,13 +516,93 @@ system.\n\n");
     }
   } //}}}
 
+  // write distribution of angles //{{{
+  // open output file for appending //{{{
+  FILE *out;
+  if ((out = fopen(output_distr, "w")) == NULL) {
+    ErrorFileOpen(output_distr, 'w');
+    exit(1);
+  } //}}}
+
+  // print command to output file //{{{
+  putc('#', out);
+  for (int i = 0; i < argc; i++)
+    fprintf(out, " %s", argv[i]);
+  putc('\n', out); //}}}
+
+  // print molecule names & bead ids //{{{
+  fprintf(out, "# dihedral angles between planes specifief by:");
+  for (int j = 0; j < number_of_beads; j += beads_per_angle) {
+    fprintf(out, " (%d) %d-%d-%d & %d-%d-%d;", j/beads_per_angle+1, dihedral[j], dihedral[j+1], dihedral[j+2], dihedral[j+3], dihedral[j+4], dihedral[j+5]);
+  }
+  putc('\n', out);
+  fprintf(out, "# columns: (1) angle [deg];");
+  int j = 2;
+  for (int i = 0; i < Counts.TypesOfMolecules; i++) {
+    if (MoleculeType[i].Use) {
+      if ((number_of_beads/beads_per_angle) == 1) {
+        fprintf(out, " (%d) %s molecules;", j, MoleculeType[i].Name);
+      } else {
+        fprintf(out, " (%d) to (%d) %s molecules;", j, j+number_of_beads/beads_per_angle-1, MoleculeType[i].Name);
+      }
+      j += number_of_beads / beads_per_angle;
+    }
+  }
+  putc('\n', out); //}}}
+
+  // write distribution to output file //{{{
+  for (int i = 0; i < bins; i++) {
+    fprintf(out, "%5.1f", width*(2*i+1)/2);
+
+    for (int j = 0; j < Counts.TypesOfMolecules; j++) {
+      if (MoleculeType[j].Use) {
+
+        for (int k = 0; k < (number_of_beads/beads_per_angle); k++) {
+          fprintf(out, "%10f", (double)(distr[j][k][i])/(count*MoleculeType[j].Number));
+        }
+      }
+    }
+    putc('\n', out);
+  } //}}}
+
+  // write to output average angles //{{{
+  fprintf(out, "# averages:");
+  j = 1;
+  for (int i = 0; i < Counts.TypesOfMolecules; i++) {
+    if (MoleculeType[i].Use) {
+      if ((number_of_beads/beads_per_angle) == 1) {
+        fprintf(out, " (%d) %s molecules;", j, MoleculeType[i].Name);
+      } else {
+        fprintf(out, " (%d) to (%d) %s molecules;", j, j+number_of_beads/beads_per_angle-1, MoleculeType[i].Name);
+      }
+      j += number_of_beads / beads_per_angle;
+    }
+  }
+  putc('\n', out);
+  for (int i = 0; i < Counts.TypesOfMolecules; i++) {
+    if (MoleculeType[i].Use) {
+      for (int j = 0; j < (number_of_beads/beads_per_angle); j++) {
+        fprintf(out, " %7.3f", avg_angle[i][j]/(count*MoleculeType[i].Number));
+      }
+    }
+  }
+  putc('\n', out); //}}}
+
+  fclose(out);
+  //}}}
+
   // free memory - to make valgrind happy //{{{
   free(BeadType);
-  FreeAggregate(Counts, &Aggregate);
   FreeMoleculeType(Counts, &MoleculeType);
   FreeMolecule(Counts, &Molecule);
   FreeBead(Counts, &Bead);
-  free(stuff); //}}}
+  free(stuff);
+  free(output);
+  for (int i = 0; i < Counts.TypesOfMolecules; i++) {
+    for (int j = 0; j < (number_of_beads/beads_per_angle); j++) {
+      free(distr[i][j]);
+    }
+  } //}}}
 
   return 0;
 }
